684ae541dbfef726468dc87cf28c1f666ce10711
YoungRJ02 <103710326+YoungRJ02@users.noreply.github.com>
2024-01-14 17:57:26 +0800

    Delete market directory

--- market/src/main/java/com/cy/market/mapper/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cy.market.mapper;
-
-import com.cy.market.entity.Order;
-import com.cy.market.entity.OrderItem;
-
-public interface OrderMapper {
-    Integer insertOrder(Order order);
-    Integer insertOrderItem(OrderItem orderItem);
-}
--- market/src/main/java/com/cy/market/service/IOrderService.java
+++
@@ -1,7 +0,0 @@
-package com.cy.market.service;
-
-import com.cy.market.entity.Order;
-
-public interface IOrderService {
-    Order create(Integer aid, Integer[] cids, Integer uid, String username);
-}
